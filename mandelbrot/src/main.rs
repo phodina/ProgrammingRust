2be2662c0a0eab9bcbc1f40a57461262480c81e0
Petr Hodina <hodinapetr46@gmail.com>
2018-10-07 14:59:48 +0200

    Add function to render a pixel.

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -60,6 +60,41 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     None
 }
 
+/// Given the row and column of a pixel in the output image, return the corresponding point on the complex plane
+///
+/// `bounds` is a pair giving the width and height of the image in pixels
+/// `pixel` is a (column, row) pair indicating a particular pixel in that image
+/// The `upper_left` and `lower_right` parameters are points on the complex plane designating the area our image covers
+fn pixel_to_point(bounds: (usize, usize), pixel: (usize,usize), upper_left: Complex<f64>, lower_right: Complex<f64>) -> Complex<f64> {
+    let (width,height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
+
+    Complex{re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+            im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64}
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100,100),(25,75), Complex{re: -1.0, im: 1.0}, Complex{re: 1.0, im: -1.0}), Complex{re: -0.5, im: -0.5});
+}
+
+/// Render a rectangle of the Mandelbrot set into a buffer of pixels.
+///
+/// The `bounds` argument gives the width and height of the buffer `pixels`, which holds one greyscale pixel per byte. The `upper_left` and `lower_right` arguments specify points on the complex plane corresponding to the upper-ledt and lower-right corners of the pixel buffer.
+fn render(pixels: &mut[u8], bounds: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) {
+
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row* bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8
+            }
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
